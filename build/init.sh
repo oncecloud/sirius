e5289e5370763181c5565d5cffdc32953392e0a7
Chen Xiaoxu <chenxiaoxu14@otcaix.iscas.ac.cn>
2017-04-12 16:07:12 +0800

    fix build scripts
    Signed-off-by: Chen Xiaoxu <chenxiaoxu14@otcaix.iscas.ac.cn>

--- build/init.sh
+++ build/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Clone the ksched repo, (for now it's a mirror repo because of access issues with private coreos repo)
-cd /root/go-workspace/src/github.com/coreos && git clone https://github.com/hasbro17/ksched-mirror ksched
+cd /root/go-workspace/src/github.com/oncecloud && git clone https://github.com/oncecloud/sirius ksched
 cd ksched/proto
 ./genproto.sh
 cd ..
